3d1f7187f51ba5e826b9efd8e5db91338884973b
Abdullah Bayindir <byndrabdullah@gmail.com>
2020-10-08 23:41:58 -0500

    button styles

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,31 +1,30 @@
-import React, {useState} from 'react'
-import Matches from '../Matches/Matches'
+import React, { useState } from "react";
+import Matches from "../Matches/Matches";
 
 function Main() {
-    
-    return (
-        <>
-        <div className="main">
-               <img src={require("../../images/logo.png")} id="logo" alt="logo"/>
-           <h1 className="title">Get the latest news of Premier League football</h1>
+  return (
+    <>
+      <div className="main">
+        <img src={require("../../images/logo.png")} id="logo" alt="logo" />
+        <h1 className="title">
+          Get the latest news of Premier League football
+        </h1>
+      </div>
 
-        </div>
-
-        <div className="page-content">
+      <div className="page-content">
         <div className="left-col">
-               <h1>left col</h1>
-           </div>
-           <div className="right-col">
-               <div className="buttons"><button>Scores</button>
-               <button>League Table</button></div>
-               <Matches/>
-           </div>
+          <h1>left col</h1>
         </div>
-           </>
-    )
+        <div className="right-col">
+          <div className="buttons">
+            <button class="btn"><i class="fas fa-futbol"></i></button>
+            <button class="btn"><i class="fas fa-table"></i></button>
+          </div>
+          <Matches />
+        </div>
+      </div>
+    </>
+  );
 }
 
-
-
-export default Main
-
+export default Main;
