c14f4a5e2fc20045cb4bc3a3a810172146125975
Abdullah Bayindir <byndrabdullah@gmail.com>
2020-10-24 00:00:34 -0500

    turned main file a class component and removed table buttons

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,77 +1,96 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import DailyMail from "../DailyMail/DailyMail";
 import Matches from "../Matches/Matches";
 import Nbc from "../Nbc/Nbc";
 import SkySports from "../SkySports/SkySports";
 import Table from "../Table/Table";
 
-function Main() {
-  const [isScores, setIsScores] = useState(true);
-  const [status, setStatus] = useState("sky")
+class Main extends Component {
+  state = {
+    isScore: "matches",
+    status: "sky",
+  };
 
-  return (
-    <>
-      <div className="main">
-        <img src={require("../../images/logo.png")} id="logo" alt="logo" />
-        <h1 className="title">
-          Get the latest news of Premier League football
-        </h1>
-      </div>
-
-      <div className="page-content">
-        <div className="left-col">
-          <div className="left-col-header">
-            <div className="news-buttons">
-              <button className={status === "sky" ? "active" : "news-btn"}
-                type="button"
-                onClick={(e) => {
-                  setStatus("sky");
-                }}>Sky Sports</button>
-              <button className={status === "nbc" ? "active" : "news-btn"}
-                type="button"
-                onClick={(e) => {
-                  setStatus("nbc");
-                }}>NBC</button>
-              <button className={status === "dm" ? "active" : "news-btn"}
-                type="button"
-                onClick={(e) => {
-                  setStatus("dm");
-                }}>Daily Mail Sports</button>
-            </div>
-          </div>
-          {status === 'sky' ? <SkySports/> : status === 'nbc' ? <Nbc/> : <DailyMail/>}
+  render() {
+    return (
+      <>
+        <div className="main">
+          <img src={require("../../images/logo.png")} id="logo" alt="logo" />
+          <h1 className="title">
+            Get the latest news of Premier League football
+          </h1>
         </div>
 
-
-        <div className="right-col">
-          <div className="buttons">
-            {isScores ? (
-              <button
-                className="btn"
-                type="button"
-                onClick={(e) => {
-                  setIsScores(false);
-                }}
-              >
-                <i className="fas fa-table"></i> Switch Table To Standings <i className="fas fa-table"></i>
-              </button>
+        <div className="page-content">
+          <div className="left-col">
+            <div className="left-col-header">
+              <div className="news-buttons">
+                <button
+                  className={
+                    this.state.status === "sky" ? "active" : "news-btn"
+                  }
+                  type="button"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    this.setState({
+                      status: "sky",
+                    });
+                  }}
+                >
+                  Sky Sports
+                </button>
+                <button
+                  className={
+                    this.state.status === "nbc" ? "active" : "news-btn"
+                  }
+                  type="button"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    this.setState({
+                      status: "nbc",
+                    });
+                  }}
+                >
+                  NBC
+                </button>
+                <button
+                  className={this.state.status === "dm" ? "active" : "news-btn"}
+                  type="button"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    this.setState({
+                      status: "dm",
+                    });
+                  }}
+                >
+                  Daily Mail Sports
+                </button>
+              </div>
+            </div>
+            {this.state.status === "sky" ? (
+              <SkySports />
+            ) : this.state.status === "nbc" ? (
+              <Nbc />
             ) : (
-              <button
-                className="btn"
-                type="button"
-                onClick={(e) => {
-                  setIsScores(true);
-                }}
-              >
-                <i className="fas fa-futbol"></i> Switch Table To Matches <i className="fas fa-futbol"></i>
-              </button>
+              <DailyMail />
             )}
           </div>
-          {isScores ? <Matches /> : <Table />}
+
+          <div className="right-col">
+            <div className="league-table">
+              <h1 className="header">League Standings</h1>
+            <Table/>
+            </div>
+              
+              <div className="matches">
+              <h1 className="header">Scores This Week</h1>
+            <Matches/>
+              </div>
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 }
 
 export default Main;
