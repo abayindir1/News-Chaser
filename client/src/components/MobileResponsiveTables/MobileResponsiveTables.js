055c3937b97b34430424976b00574e377999058e
Abdullah Bayindir <byndrabdullah@gmail.com>
2020-10-29 23:11:45 -0500

    mobile phones css

--- client/src/components/MobileResponsiveTables/MobileResponsiveTables.js
+++ client/src/components/MobileResponsiveTables/MobileResponsiveTables.js
@@ -5,20 +5,29 @@ import Table from "../Table/Table";
 export default function MobileResponsiveTables() {
   const [hidden, setHidden] = useState(true);
 
+  function changeHidden (){
+    if(hidden){
+      setHidden(false)
+    }else{
+      setHidden(true)
+    }
+  }
+
   return (
     <div className="responsive-tables">
-      <button className="responsive-button">
-        <i class="fas fa-angle-double-up"></i> Tables{" "}
-        <i class="fas fa-angle-double-up"></i>
+      <button className="responsive-button" onClick={()=> changeHidden()}>
+        {hidden ? (<h3><i class="fas fa-angle-double-up"></i> Tables</h3>) : <h3><i class="fas fa-angle-double-down"></i> Tables</h3>}
       </button>
 
-      <div className={hidden ? "responsive-content-hidden" : "responsive-content"}>
+      <div
+        className={hidden ? "responsive-content-hidden" : "responsive-content"}
+      >
         <div className="matches-responsive">
           <Matches />
         </div>
 
         <div className="standings-responsive">
-            <Table/>
+          <Table />
         </div>
       </div>
     </div>
